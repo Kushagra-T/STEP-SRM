94a350841e31b7e73618470317bdf5f3324d4ec5
Kushagra-T <kushagrasty@gmail.com>
2025-02-05 23:26:20 +0530

    Level-1 Question-1

---
+++ L1Q1.java
@@ -0,0 +1,18 @@
+// Creating Class with name L1Q1 indicating the purpose is to display age of Harry
+class L1Q1
+{
+    public static void main(String args[])
+    {
+        // Create a int variable py to indicate the present year
+        int presentyear=2024;
+
+        // Create a int variable by to indicate the birth year
+        int birthyear=2000;
+
+        // Create a int variable a to indicate the age
+        int age=presentyear-birthyear;
+
+        // Display the age
+        System.out.println("Harry's age in 2024 is "+age);
+    }
+}
